d1404d382a93a788fd83e092e948cc08736f1d4d
Joao P Dubas <joao.dubas@gmail.com>
2020-02-10 01:06:12 +0000

    chore(storage) create partner from raw value

--- pkg/storage/errors.go
+++ pkg/storage/errors.go
@@ -6,5 +6,6 @@ var (
 	ErrorDuplicateID       = errors.New("duplicated id")
 	ErrorDuplicateDocument = errors.New("duplicated document")
 	ErrorNotFound          = errors.New("id not found")
+	ErrorWrongCoverageArea = errors.New("wrong type for coverage area")
 	ErrorWrongAddress      = errors.New("wrong type for address")
 )
--- pkg/storage/partner.go
+++ pkg/storage/partner.go
@@ -1,6 +1,10 @@
 package storage
 
-import geojson "github.com/paulmach/go.geojson"
+import (
+	"fmt"
+
+	geojson "github.com/paulmach/go.geojson"
+)
 
 type Partner struct {
 	ID           int              `json:"id"`
@@ -9,4 +13,31 @@ type Partner struct {
 	Document     string           `json:"document"`
 	CoverageArea geojson.Geometry `json:"coverageArea"`
 	Address      geojson.Geometry `json:"address"`
+}
+
+func NewPartner(id int, tradingName, ownerName, document string, coverageArea, address []byte) (Partner, error) {
+	p := Partner{
+		ID:          id,
+		TradingName: tradingName,
+		OwnerName:   ownerName,
+		Document:    document,
+	}
+
+	coverageGeom, err := geojson.UnmarshalGeometry(coverageArea)
+	if err != nil {
+		return p, fmt.Errorf("NewPartner: error converting coverage area (%w)", err)
+	} else if !coverageGeom.IsMultiPolygon() && !coverageGeom.IsPolygon() {
+		return p, fmt.Errorf("NewPartner: invalid coverage area (%w)", ErrorWrongCoverageArea)
+	}
+
+	addressGeom, err := geojson.UnmarshalGeometry(address)
+	if err != nil {
+		return Partner{}, fmt.Errorf("NewPartner: error coverting address (%w)", err)
+	} else if !addressGeom.IsPoint() {
+		return p, fmt.Errorf("NewPartner: invalid address (%w)", ErrorWrongAddress)
+	}
+
+	p.CoverageArea = *coverageGeom
+	p.Address = *addressGeom
+	return p, nil
 }
